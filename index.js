c667ddfe6b44e1209b45399c61e1e55dbd9d67bb
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-08-01 14:40:11 +0200

    Changed the diplay

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+//import the greet module that is in the current folder
+const Greet = require('./greet');
+const greetUser = Greet();
+
 let express = require('express');
 let app = express();
 
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-
-//Require my factory function ........
-
-
-
 
 //Static Resource
 app.use(express.static('public'));
@@ -29,6 +27,32 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
+
+//This Route should accept the username as part of the url.
+app.post("/greetings/:user", function(req, res)
+{
+    let userPerson = req.body.user;
+    let languageChoice = req.body.language;
+
+
+});
+
+app.get("/greeted", function(req,res){
+
+
+
+});
+
+app.get("/counter/:username", function(req,res){
+
+
+
+});
+
+
+
+
+//-------------------------------------------//
 let PORT = process.env.PORT || 3312;
 
 app.listen(PORT, function(){
