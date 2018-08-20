87af2a1ec3eddc1a490c43eda921626c3f2cb8f3
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-08-20 14:54:22 +0200

    Added the mocha dependency for testing

--- test/greet.test.js
+++ test/greet.test.js
@@ -7,27 +7,36 @@ let greetingPerson = require('../greet.js');
 const pg = require("pg");
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/usersDB';
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/usersDB';
 
 const pool = new Pool({
     connectionString
 });
 
-describe('The Greeting WebApp Database Unit Tests', function() 
+describe('The Greeting WebApp Database Unit Tests', async function() 
 {
-      beforeEach(async function(){
-        await pool.query("delete from users;");
-    });
+    beforeEach(async function(){await pool.query("delete from users;");});
 
      it('It should Greet the user(Siphe) with the proper language and name (i.e Molo, Siphe)',
-    function() {
-      var greetings = await greetingPerson({});
-
-      greetings.setname("Siphe");
-      greetings.set_language("Isixhosa");
-
-      assert.equal(greetings.doGreet(),'Molo, Siphe');
-  
+       async function() 
+      {
+          let greetings = await greetingPerson(pool);
+          assert.equal(await greetings.greet("Siphe","Isixhosa" ),'Molo, Siphe');
      });
+
+     it('It should Greet the user(Mbuso) with the proper language and name (i.e Hello, Mbuso)',
+     async function() 
+    {
+        let greetings = await greetingPerson(pool);
+        assert.equal(await greetings.greet("Mbuso","English" ),'Hello, Mbuso');
+   });
+
+   it('It should Greet the user(Siba) with the proper language and name (i.e Hallo, Siba)',
+   async function() 
+  {
+      let greetings = await greetingPerson(pool);
+      assert.equal(await greetings.greet("Siba","Afrikaans" ),'Hallo, Siba');
+ });
+
 
   });
